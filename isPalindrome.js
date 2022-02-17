5461b872e9ae74a9d2b2a22329391f5abae45a2c
TYama <alohaty81@gmail.com>
2022-02-16 18:27:57 -0700

    made a procedure for this exercise

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,17 @@
 // Write your code below
+
+//defalut isPalindrome = true.
+//check if the text length is even or odd.
+//if odd -> get the middle index of the text.
+//if it is even check the characters from both ends.
+//if it is even, chec the characters from both ends but skip the middle index.
+//if there is any unmatched chars, return isPandrome = false and break out of the function.
+
+let isPalindrome = (word) => {
+    
+    return true;
+}
+
+let isWordLengthEven = wordLength => wordLength % 2 === 0;
+
+let wordMiddleIndex = wordLength => Math.floor(wordLength / 2);
